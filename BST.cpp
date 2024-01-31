013371ea078b02a1c2a75c0b8f7f8179b2f1d6b3
Horad <iranradmard1@gmail.com>
2024-01-31 06:33:01 +0330

    Edit

--- BST.cpp
+++ BST.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// In binary trees every node has three part: value, right node and left node
 class Node
 {
 
-private:
+    // private:
+public:
     double value;
     Node *right;
     Node *left;
-
-public:
     Node()
     {
         value = -9999;
         right = nullptr;
         left = nullptr;
     }
-    Node(double &val)
+    Node(double val)
     {
         value = val;
         right = nullptr;
@@ -26,48 +24,90 @@ public:
 
     friend class Tree;
 };
-
 // Every tree defines by it's root if we have a root we can navigate all nodes
 class Tree
 {
-private:
-    Node *root;
-
+    // private:
 public:
+    Node *root;
     Tree()
     {
         root = nullptr;
     }
-    void Create_tree(double &val){
+    Tree(double val)
+    {
         root = new Node(val);
-        return;
     }
-    void Insert_node(double &val){
-        Node* nav = root;
-        Node* pre;
 
+    void Insert_node(double val)
+    {
+        Node *nav = root;
+        Node *pre;
 
-        if(nav == nullptr)
-            Create_tree(val);
-        else{
-            while(nav != nullptr){
+        if (root == nullptr)
+        {
+            root = new Node(val);
+        }
+        else
+        {
+            while (nav != nullptr)
+            {
                 pre = nav;
-                if(val >= nav->value)
-                    nav = nav->right
+                if (val >= nav->value)
+                    nav = nav->right;
                 else
-                    nav = nav->left
+                    nav = nav->left;
             }
 
-            new_node = new Node(val);
-            if(val >= pre->value) pre->right = new_node;
-            else pre->left = new_node;
+            Node *new_node = new Node(val);
+            if (val >= pre->value)
+                pre->right = new_node;
+            else
+                pre->left = new_node;
         }
         delete nav, pre;
+        return;
     }
 };
 
+bool BST(Tree T, double val);
+
 int main()
 {
-    Tree a1;
+    Tree T1;
+
+    for (size_t i = 0; i < 5; i++)
+        T1.Insert_node(i - 1);
+
+    cout << BST(T1, 3);
     return 0;
+}
+
+// In binary trees every node has three part: value, right node and left node
+bool BST(Tree T, double val)
+{
+    Node *nav = T.root;
+
+    if (nav == nullptr)
+    {
+        cout << "\nTree is empty\n";
+        return 0;
+    }
+
+    while (nav != nullptr)
+    {
+        if (nav->value == val)
+        {
+            cout << "\nfounded!\n";
+            return 1;
+        }
+        else if (val > nav->value)
+            nav = nav->right;
+        else
+            nav = nav->left;
+    }
+    cout << "\nnot found!\n";
+    return 0;
+
+    delete nav;
 }
