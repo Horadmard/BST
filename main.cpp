3d21f06aa679fbac5c7759cdb4841b74630fa07b
Horad <iranradmard1@gmail.com>
2024-01-31 06:54:54 +0330

    Test

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@ int main()
 {
     Tree T1;
 
-    for (size_t i = 0; i < 5; i++)
-        T1.Insert_node(i - 1);
+    for (size_t i = 0; i < 1000; i++)
+        T1.Insert_node(i);
 
-    Binary_Search(T1, 3);
+    Binary_Search(T1, 500);
+    Binary_Search(T1, 1500);
     return 0;
 }
 
